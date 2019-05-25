2b27ac6f4cf8dc2597a5a540d9754385084336bf
stevekutz <stkutz@gmail.com>
2019-05-24 23:48:24 -0500

    200, 404, 500 and messages verified with DELETE

--- data/data-router.js
+++ data/data-router.js
@@ -123,7 +123,31 @@ router.post('/:id/comments', async (req, res) => {
 
 
 // DELETE(tricky,extra calls!) Removes post with specified id and RETURNS deleted post 
+router.delete('/:id', async (req, res) => {
 
+    const post = await Posts.findById(req.params.id);
+
+    if(post){
+        try {
+        const count = await Posts.remove(req.params.id);
+
+        if (count > 0) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ 
+                message: 'The post with the specified ID does not exist.' 
+            });
+        }
+        } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            message: 'The post could not be removed',
+        });
+        }
+    }
+    
+  });
 
 
 // PUT updates post with specific id as packaged via request body, return UPDATED post array
